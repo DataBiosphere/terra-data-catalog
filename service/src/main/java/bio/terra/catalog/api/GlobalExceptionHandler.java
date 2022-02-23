859e650afa4d5f4a8a09b477d745eb0bae1b6fb5
Phil Shapiro <pshapiro@broadinstitute.org>
2022-02-23 15:08:01 -0500

    Fix exception handler to use correct error model for response

--- service/src/main/java/bio/terra/catalog/api/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/catalog/api/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
 package bio.terra.catalog.api;
 
+import bio.terra.catalog.model.ErrorReport;
 import bio.terra.common.exception.UnauthorizedException;
-import bio.terra.datarepo.model.ErrorModel;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import javax.annotation.Nonnull;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,27 +16,23 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(UnauthorizedException.class)
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
-  public ErrorModel samAuthorizationException(UnauthorizedException e) {
-    return buildErrorModel(e);
+  public ErrorReport samAuthorizationException(UnauthorizedException e) {
+    return buildErrorReport(e);
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ErrorModel backstopHandler(Exception e) {
+  public ErrorReport backstopHandler(Exception e) {
     logger.error("Exception caught by backstop handler", e);
-    return buildErrorModel(e);
+    return buildErrorReport(e);
   }
 
-  private ErrorModel buildErrorModel(Throwable ex) {
-    return buildErrorModel(ex, null);
-  }
-
-  private ErrorModel buildErrorModel(Throwable e, List<String> errorDetail) {
+  private ErrorReport buildErrorReport(@Nonnull Throwable e) {
     StringBuilder combinedCauseString = new StringBuilder();
     for (Throwable cause = e; cause != null; cause = cause.getCause()) {
       combinedCauseString.append("cause: ").append(cause).append(", ");
     }
     logger.error("Global exception handler: " + combinedCauseString, e);
-    return new ErrorModel().message(e.getMessage()).errorDetail(errorDetail);
+    return new ErrorReport(e.getMessage());
   }
 }
