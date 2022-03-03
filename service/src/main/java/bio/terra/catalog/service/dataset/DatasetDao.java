1c62949ce2202ca8c2afc8d225492eef52b95c5c
Saman Ehsan <se8ea@virginia.edu>
2022-03-03 15:23:36 -0500

    Update dataset dao
    Handle dataset not found error
    and make the create method return
    the dataset.

--- service/src/main/java/bio/terra/catalog/service/dataset/DatasetDao.java
+++ service/src/main/java/bio/terra/catalog/service/dataset/DatasetDao.java
@@ -1,19 +1,22 @@
 package bio.terra.catalog.service.dataset;
 
+import bio.terra.catalog.common.DaoKeyHolder;
 import bio.terra.catalog.common.StorageSystem;
+import bio.terra.catalog.service.dataset.exception.DatasetNotFoundException;
 import bio.terra.catalog.service.dataset.exception.InvalidDatasetException;
 import bio.terra.common.db.ReadTransaction;
 import bio.terra.common.db.WriteTransaction;
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -38,11 +41,15 @@ public class DatasetDao {
     String sql =
         "SELECT id, dataset_id, storage_system, metadata, created_date FROM dataset WHERE id = :id";
     MapSqlParameterSource params = new MapSqlParameterSource().addValue("id", id);
-    return jdbcTemplate.queryForObject(sql, params, new DatasetMapper());
+    try {
+      return jdbcTemplate.queryForObject(sql, params, new DatasetMapper());
+    } catch (EmptyResultDataAccessException ex) {
+      throw new DatasetNotFoundException("Dataset not found for id " + id);
+    }
   }
 
   @WriteTransaction
-  public void create(Dataset dataset) throws IOException {
+  public Dataset create(Dataset dataset) {
     String sql =
         "INSERT INTO dataset (dataset_id, storage_system, metadata) "
             + "VALUES (:dataset_id, :storage_system, cast(:metadata as jsonb))";
@@ -51,12 +58,21 @@ public class DatasetDao {
             .addValue("dataset_id", dataset.datasetId())
             .addValue("storage_system", String.valueOf(dataset.storageSystem()))
             .addValue("metadata", dataset.metadata());
+    DaoKeyHolder keyHolder = new DaoKeyHolder();
     try {
-      jdbcTemplate.update(sql, params);
+      jdbcTemplate.update(sql, params, keyHolder);
     } catch (DuplicateKeyException e) {
       throw new InvalidDatasetException(
           "A dataset for this dataset_id and storage_system already exists");
     }
+
+    return new Dataset(
+        keyHolder.getId(),
+        keyHolder.getString("dataset_id"),
+        StorageSystem.valueOf(keyHolder.getString("storage_system")),
+        keyHolder.getString("metadata"),
+        keyHolder.getTimestamp("created_date").toInstant()
+    );
   }
 
   @WriteTransaction
