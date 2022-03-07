c91085ffbd57ef03a51d64a4f99043f1adc5c09f
Saman Ehsan <se8ea@virginia.edu>
2022-03-07 13:48:24 -0500

    Fix spotless again

--- service/src/main/java/bio/terra/catalog/service/dataset/exception/InvalidDatasetException.java
+++ service/src/main/java/bio/terra/catalog/service/dataset/exception/InvalidDatasetException.java
@@ -1,7 +1,6 @@
 package bio.terra.catalog.service.dataset.exception;
 
 import bio.terra.common.exception.BadRequestException;
-import java.util.List;
 
 public class InvalidDatasetException extends BadRequestException {
   public InvalidDatasetException(String message, Throwable cause) {
