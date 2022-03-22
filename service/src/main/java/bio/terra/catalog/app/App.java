ebc8784e8a06703f436706f39074a144b0651f5b
Stephen Lucas <slucas@wmd5c-4c2.broadinstitute.org>
2022-03-22 16:46:19 -0400

    continuing to solve mystories of swagger

--- service/src/main/java/bio/terra/catalog/app/App.java
+++ service/src/main/java/bio/terra/catalog/app/App.java
@@ -1,6 +1,8 @@
 package bio.terra.catalog.app;
 
 import bio.terra.common.logging.LoggingInitializer;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -27,6 +29,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
       // Scan all service-specific packages beneath the current package
       "bio.terra.catalog"
     })
+@OpenAPIDefinition(info = @Info(title = "Terra", version = "2.0"))
+// @ConfigurationPropertiesScan("bio.terra.catalog")
 @ConfigurationPropertiesScan("bio.terra.catalog")
 @EnableRetry
 @EnableTransactionManagement
---
+++ service/src/test/java/bio/terra/catalog/api/AppSwaggerTest.java
@@ -0,0 +1,16 @@
+package bio.terra.catalog.api;
+
+import bio.terra.catalog.app.App;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = App.class)
+public class AppSwaggerTest {
+  @Test
+  void contextLoads() {}
+
+  //  public void contextLoads() throws Exception {
+  //    assertThat(controller).isNotNull();
+  //  }
+
+}
