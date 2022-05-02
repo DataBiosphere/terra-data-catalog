8963015f666b318a7d51a2887a804f94f1839a10
Phil Shapiro <pshapiro@broadinstitute.org>
2022-05-02 16:24:09 -0400

    Revert "WIP ingest-hca-collection.py changes / updates"
    This reverts commit 9cc02cc043e1d97f0cac618b60c57b83cf07d563.

--- scripts/hca/ingest-hca-collection.py
+++ scripts/hca/ingest-hca-collection.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
-import json
-import os
-
-import requests
+import json, os
+from urllib.request import Request, urlopen
 from tqdm import tqdm
 
 upsert_url = "https://catalog.dsde-dev.broadinstitute.org/api/v1/datasets"
-policy_url = (
-    "https://jade.datarepo-dev.broadinstitute.org/"
-    "api/repository/v1/snapshots/{id}/policies/steward/members"
-)
+policy_url = "https://jade.datarepo-dev.broadinstitute.org/api/repository/v1/snapshots/{id}/policies/steward/members"
 
 
 def auth_token():
@@ -21,10 +16,10 @@ def user_email():
     return os.environ["USER_EMAIL"]
 
 
-def api_request(snapshot_id, url, obj, method):
+def api_request(id, url, obj, method):
     auth, token = auth_token()
 
-    url = url.format(id=snapshot_id)
+    url = url.format(id=id)
 
     data = None
     if obj:
@@ -36,45 +31,35 @@ def api_request(snapshot_id, url, obj, method):
         auth: token,
     }
 
-    req = requests.request(method, url, headers=headers, data=data)
+    req = Request(url, data=data, headers=headers, method=method)
+    res = urlopen(req)
 
-    if not req.ok and method != "GET":
-        raise Exception(f"Error: {req}")
-
-    return req.ok
+    return res.getcode()
 
 
-def add_policy(snapshot):
+def policy(snapshot):
     email = {"email": user_email()}
     api_request(snapshot["dct:identifier"], policy_url, email, "POST")
 
 
-def remove_policy(snapshot):
-    snapshot_id = snapshot["dct:identifier"]
-    api_request(snapshot_id, policy_url + "/" + user_email(), None, "DELETE")
-
-
 def upsert(snapshot):
-    snapshot_id = snapshot["dct:identifier"]
     body = {
         "storageSystem": "tdr",
-        "storageSourceId": snapshot_id,
+        "storageSourceId": snapshot["dct:identifier"],
         "catalogEntry": json.dumps(snapshot),
     }
 
-    # If it doesn't exist, add it.
-    if not api_request(snapshot_id, upsert_url + "/{id}", None, "GET"):
-        api_request(snapshot_id, upsert_url, body, "POST")
+    # don't add anything if there's already an entry
+    if api_request(snapshot["dct:identifier"], upsert_url, None, "GET") == 200:
+        return
+
+    api_request(snapshot["dct:identifier"], upsert_url, body, "POST")
 
 
-def main():
-    with open("hca-collection.json", "r") as f:
-        collection = json.load(f)
+with open("hca-collection.json", "r") as f:
+    collection = json.load(f)
 
-    for snapshot in tqdm(collection["data"]):
-        add_policy(snapshot)
-        upsert(snapshot)
-        remove_policy(snapshot)
-
-
-main()
+# TODO: remove steward policy after upsert?
+for snapshot in tqdm(collection["data"]):
+    policy(snapshot)
+    upsert(snapshot)
