7c5b3d1272b02c72fd200b684b5fdb5a19051673
Florian Boulnois <fboulnoi@broadinstitute.org>
2022-05-06 16:51:10 -0400

    Add Rawls status tests

--- service/src/main/java/bio/terra/catalog/rawls/RawlsService.java
+++ service/src/main/java/bio/terra/catalog/rawls/RawlsService.java
@@ -4,17 +4,28 @@ import bio.terra.catalog.config.RawlsConfiguration;
 import bio.terra.catalog.model.SystemStatusSystems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public record RawlsService(RawlsConfiguration rawlsConfig) {
+public class RawlsService {
   private static final Logger logger = LoggerFactory.getLogger(RawlsService.class);
+  private final RawlsConfiguration rawlsConfig;
+
+  @Autowired
+  public RawlsService(RawlsConfiguration rawlsConfig) {
+    this.rawlsConfig = rawlsConfig;
+  }
+
+  public boolean testRawlsStatus() throws Exception {
+    return true;
+  }
 
   public SystemStatusSystems status() {
     var result = new SystemStatusSystems();
     try {
       // TODO: actually check Rawls status. Does this need the Rawls API client?
-      result.ok(true);
+      result.ok(testRawlsStatus());
       if (!result.isOk()) {
         String errorMsg = "Rawls status check failed.";
         logger.error(errorMsg);
---
+++ service/src/test/java/bio/terra/catalog/rawls/RawlsServiceTest.java
@@ -0,0 +1,61 @@
+package bio.terra.catalog.rawls;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import bio.terra.catalog.model.SystemStatusSystems;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ConfigurationPropertiesScan("bio.terra.catalog")
+@ContextConfiguration(classes = {RawlsService.class})
+class RawlsServiceTest {
+  @Autowired private RawlsService rawlsServiceReal;
+
+  private RawlsService rawlsService;
+
+  @BeforeEach
+  void beforeEach() {
+    rawlsService = spy(rawlsServiceReal);
+  }
+
+  @Test
+  void status() throws Exception {
+    SystemStatusSystems status = new SystemStatusSystems().ok(true);
+    when(rawlsService.status()).thenReturn(status);
+    var rawlsStatus = rawlsService.status();
+    assertTrue(rawlsStatus.isOk());
+  }
+
+  @Test
+  void statusDown() throws Exception {
+    SystemStatusSystems status = new SystemStatusSystems().ok(false);
+    when(rawlsService.status()).thenReturn(status);
+    var rawlsStatus = rawlsService.status();
+    assertFalse(rawlsStatus.isOk());
+  }
+
+  @Test
+  void statusDown2() throws Exception {
+    when(rawlsService.testRawlsStatus()).thenReturn(false);
+    var rawlsStatus = rawlsService.status();
+    assertFalse(rawlsStatus.isOk());
+  }
+
+  @Test
+  void statusException() throws Exception {
+    when(rawlsService.testRawlsStatus()).thenThrow(new Exception());
+    var rawlsStatus = rawlsService.status();
+    assertFalse(rawlsStatus.isOk());
+  }
+}
