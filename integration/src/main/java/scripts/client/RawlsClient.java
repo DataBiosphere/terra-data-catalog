8ab06fe3ef43901ac0fbb42f8ff83e0ec87d5b89
Phil Shapiro <pshapiro@broadinstitute.org>
2022-05-09 12:53:35 -0400

    possible rawls client generation / use in integration test

---
+++ integration/src/main/java/scripts/client/RawlsClient.java
@@ -0,0 +1,36 @@
+package scripts.client;
+
+import bio.terra.rawls.client.ApiClient;
+import bio.terra.testrunner.common.utils.AuthenticationUtils;
+import bio.terra.testrunner.runner.config.ServerSpecification;
+import bio.terra.testrunner.runner.config.TestUserSpecification;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.IOException;
+import java.util.Objects;
+
+public class RawlsClient extends ApiClient {
+  /**
+   * Build the API client object for the given test user and catalog server. The test user's token
+   * is always refreshed. If a test user isn't configured (e.g. when running locally), return an
+   * un-authenticated client.
+   *
+   * @param server the server we are testing against
+   * @param testUser the test user whose credentials are supplied to the API client object
+   */
+  public RawlsClient(ServerSpecification server, TestUserSpecification testUser)
+      throws IOException {
+    // FIXME
+//    setBasePath(Objects.requireNonNull(server.rawlsUri, "Rawls URI required"));
+
+    if (testUser != null) {
+      GoogleCredentials userCredential =
+          AuthenticationUtils.getDelegatedUserCredential(
+              testUser, AuthenticationUtils.userLoginScopes);
+      var accessToken = AuthenticationUtils.getAccessToken(userCredential);
+      if (accessToken != null) {
+        setAccessToken(accessToken.getTokenValue());
+      }
+    }
+  }
+
+}
