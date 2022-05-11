6c8255e15361500c049e7096a49adc18da134abe
Phil Shapiro <pshapiro@broadinstitute.org>
2022-05-11 16:35:59 -0400

    Add a unit test for DatarepoClient

---
+++ service/src/test/java/bio/terra/catalog/datarepo/DatarepoClientTest.java
@@ -0,0 +1,43 @@
+package bio.terra.catalog.datarepo;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import bio.terra.catalog.config.DatarepoConfiguration;
+import bio.terra.common.iam.AuthenticatedUserRequest;
+import bio.terra.datarepo.client.auth.OAuth;
+import org.junit.jupiter.api.Test;
+
+class DatarepoClientTest {
+
+  private static final String BASE_PATH = "basepath";
+  private static final String TOKEN = "token";
+
+  private final DatarepoClient client;
+
+  DatarepoClientTest() {
+    client = new DatarepoClient(new DatarepoConfiguration(BASE_PATH));
+  }
+
+  @Test
+  void testApis() {
+    var user =
+        new AuthenticatedUserRequest.Builder()
+            .setEmail("")
+            .setSubjectId("")
+            .setToken(TOKEN)
+            .build();
+    var snapshotsClient = client.snapshotsApi(user).getApiClient();
+    assertThat(snapshotsClient.getBasePath(), is(BASE_PATH));
+
+    OAuth oauth = (OAuth) snapshotsClient.getAuthentication("googleoauth");
+    assertNotNull(oauth);
+    assertThat(oauth.getAccessToken(), is(TOKEN));
+
+    var unauthClient = client.unauthenticatedApi().getApiClient();
+    assertThat(unauthClient.getBasePath(), is(BASE_PATH));
+
+    assertThat(unauthClient.getHttpClient(), is(snapshotsClient.getHttpClient()));
+  }
+}
