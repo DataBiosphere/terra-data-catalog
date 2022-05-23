5f7788457f9fab949d8e0d2aa21fe400b60d8cd6
Sky Rubenstein <blrubenstein@wpi.edu>
2022-05-23 14:51:56 -0400

    Remove unused import

--- service/src/test/java/bio/terra/catalog/rawls/RawlsServiceTest.java
+++ service/src/test/java/bio/terra/catalog/rawls/RawlsServiceTest.java
@@ -2,7 +2,6 @@ package bio.terra.catalog.rawls;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
