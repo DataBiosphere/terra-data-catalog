22b3f6a139f4bc2f46f103f574c9d4195f751536
Phil Shapiro <pshapiro@broadinstitute.org>
2022-06-28 10:03:11 -0400

    DC-415: link to workspace support; add workspace URL to dataset metadata (#102)
    DC-415: add link to terra workspace for workspace datasets, also:
    
    - update accessURL generated by HCA script
    - update to current HCA catalog version
    - replace `RAWLS_URL` with `ENV`

--- scripts/workspace-conversion/workspace-to-dataset.py
+++ scripts/workspace-conversion/workspace-to-dataset.py
@@ -16,23 +16,22 @@
 #
 # Run:
 #   `python3 workspace-to-dataset.py`
-#   `export GCLOUD_USER={your-email}; export WORKSPACE_NAMESPACE={workspace-namespace}; export WORKSPACE_NAME={workspace-name}; python3 workspace-to-dataset.py`
+#   `WORKSPACE_NAMESPACE={workspace-namespace} WORKSPACE_NAME={workspace-name} python3 workspace-to-dataset.py`
 #
 # Environment Variables:
-#   GCLOUD_USER: user account email
 #   WORKSPACE_NAMESPACE: workspace namespace for the rawls query
 #   WORKSPACE_NAME: workspace name for the rawls query
-#   RAWLS_URL: url for rawls. Default set to rawls-prod
-#   TERRA_URL: url for terra ui. Default set to staging. Used to output demo link.
+#   GCLOUD_USER: user account email; default is `datacatalogadmin@test.firecloud.org`
+#   ENV: terra environment to get workspace data from; default is `prod`
 # ------------------------------------------------------------------------------
 import json
 import os, subprocess
+
 import requests
 import itertools
-from datetime import datetime
 
-urlRoot = os.environ.get("RAWLS_URL") or "https://rawls.dsde-prod.broadinstitute.org"
-urlWorkspace = f"{urlRoot}/api/workspaces"
+env = os.environ.get("ENV") or "prod"
+urlWorkspace = f"https://rawls.dsde-{env}.broadinstitute.org/api/workspaces"
 workspaceNamespace = os.environ.get("WORKSPACE_NAMESPACE")
 workspaceName = os.environ.get("WORKSPACE_NAME")
 user = os.environ.get("GCLOUD_USER") or "datacatalogadmin@test.firecloud.org"
@@ -266,6 +265,18 @@ def get_workspace_files(wsAttributes):
     return ret
 
 
+def access_url(workspace):
+    terra_url = (
+        "https://app.terra.bio"
+        if env == "prod"
+        else f"https://bvdp-saturn-{env}.appspot.com"
+    )
+
+    w = workspace["workspace"]
+
+    return f"{terra_url}/#workspaces/{w['namespace']}/{w['name']}"
+
+
 def generate_catalog_metadata(workspace, bucket):
     print("Generating workspace metadata")
 
@@ -314,6 +325,7 @@ def generate_catalog_metadata(workspace, bucket):
         "dct:version": wsAttributes.pop("library:datasetVersion", None),
         "dct:description": wsAttributes.pop("library:datasetDescription", None),
         "dct:modified": workspace["workspace"]["lastModified"],
+        "dcat:accessURL": access_url(workspace),
         "TerraDCAT_ap:hasDataCollection": list(
             filter(
                 None,
