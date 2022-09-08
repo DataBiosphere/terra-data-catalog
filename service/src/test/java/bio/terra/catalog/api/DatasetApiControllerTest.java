05a427c19782901181dfe98844d885ee514d5b8a
Phil Shapiro <pshapiro@broadinstitute.org>
2022-09-08 16:35:56 -0400

    Minor cleanups to test annotations

--- service/src/test/java/bio/terra/catalog/api/DatasetApiControllerTest.java
+++ service/src/test/java/bio/terra/catalog/api/DatasetApiControllerTest.java
@@ -34,18 +34,14 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(
-    classes = {DatasetApiController.class, DatasetService.class, GlobalExceptionHandler.class})
+@ContextConfiguration(classes = {DatasetApiController.class, GlobalExceptionHandler.class})
 @WebMvcTest
 class DatasetApiControllerTest {
 
