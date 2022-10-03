e68f18f5632da54c159dafeb16521360cec8ee48
Phil Shapiro <pshapiro@broadinstitute.org>
2022-10-03 14:24:41 -0400

    Used `classpath:` and now things work?!

--- service/src/test/java/bio/terra/catalog/service/dataset/DatasetDaoTest.java
+++ service/src/test/java/bio/terra/catalog/service/dataset/DatasetDaoTest.java
@@ -15,8 +15,10 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class DatasetDaoTest {
 
   @Autowired private DatasetDao datasetDao;
