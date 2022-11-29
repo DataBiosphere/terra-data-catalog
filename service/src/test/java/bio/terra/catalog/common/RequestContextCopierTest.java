da7791c61137dc28824e50d25af529836ef7691c
Sky Rubenstein <blrubenstein@wpi.edu>
2022-11-29 14:59:03 -0500

    Change form comment to disabled value

--- service/src/test/java/bio/terra/catalog/common/RequestContextCopierTest.java
+++ service/src/test/java/bio/terra/catalog/common/RequestContextCopierTest.java
@@ -32,10 +32,9 @@ class RequestContextCopierTest {
   }
 
   @Test
-  @Disabled
-  // This is disabled because the way in which Java optimizes parallelization is tricky to test in
-  // junit. It works consistently in the positive test, but in the negative test it sometimes
-  // optimizes things away where we would like it not to.
+  @Disabled(
+      value =
+          "This is disabled because the way in which Java optimizes parallelization is tricky to test in junit. It works consistently in the positive test, but in the negative test it sometimes optimizes things away where we would like it not to.")
   void requestContextDoesntFollowToChildThreads() {
     assertThrows(IllegalStateException.class, () -> runTestWithProvider(Stream::parallel));
   }
