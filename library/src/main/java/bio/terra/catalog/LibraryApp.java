80abca8e5a4282930d2fd3c936dfba3e1fbf97b9
Phil Shapiro <pshapiro@broadinstitute.org>
2022-12-20 10:07:21 -0500

    sonar: use logging, not `System.err`

--- library/src/main/java/bio/terra/catalog/LibraryApp.java
+++ library/src/main/java/bio/terra/catalog/LibraryApp.java
@@ -1,5 +1,7 @@
 package bio.terra.catalog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -24,8 +26,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class LibraryApp {
 
+  private static final Logger logger = LoggerFactory.getLogger(LibraryApp.class);
+
   public static void main(String[] args) {
-    System.err.println("The library app can't be run.");
+    logger.error("The library app can't be run.");
     System.exit(-1);
   }
 }
